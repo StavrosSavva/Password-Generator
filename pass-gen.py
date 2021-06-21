fa06801b0cac8b944b1f37e225f60b1b40b8e2fa
StavrosSavva <s.savva@outlook.com>
2021-06-21 14:14:23 +0100

    File ready to use

---
+++ pass-gen.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+length = int(input('\nEnter the length of password: '))
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+
+all = lower + upper + num + symbols
+
+#random select 
+temp = random.sample(all,length)
+
+password = "".join(temp)
+
+print("New Password: " + password)
